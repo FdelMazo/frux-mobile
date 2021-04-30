acbde8b9796ea51bdb4714f63bccd1004edf12cd
FdelMazo <fdelmazo@fi.uba.ar>
2021-04-30 16:47:25 -0300

    Add login and signin methods

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import firebase from "firebase/app";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import firebaseConfig from "./config/firebase";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
 
+  const colorScheme = useColorScheme();
   if (!isLoadingComplete) {
     return null;
   } else {
---
+++ auth.tsx
@@ -0,0 +1,35 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { Alert } from "react-native";
+
+export async function registration(email, password, username) {
+  try {
+    await firebase.auth().createUserWithEmailAndPassword(email, password);
+    const currentUser = firebase.auth().currentUser;
+
+    const db = firebase.firestore();
+    db.collection("users").doc(currentUser.uid).set({
+      email: currentUser.email,
+      username,
+    });
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
+export async function signIn(email, password) {
+  try {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+  } catch (err) {
+    alert(err.message);
+  }
+}
+
+export async function loggingOut() {
+  try {
+    await firebase.auth().signOut();
+  } catch (err) {
+    Alert.alert("There is something wrong!", err.message);
+  }
+}
