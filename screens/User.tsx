533f88941853fd288129426e234849bf19c4638b
FdelMazo <fdelmazo@fi.uba.ar>
2021-06-05 16:51:30 -0300

    Add set user location module

--- screens/User.tsx
+++ screens/User.tsx
@@ -1,8 +1,17 @@
 import gql from "graphql-tag";
 import * as React from "react";
 import { useMutation, useQuery } from "react-apollo";
-import { FlatList } from "react-native-gesture-handler";
-import { Button, Div, Dropdown, Icon, Input, Text } from "react-native-magnus";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import {
+  Button,
+  Div,
+  Dropdown,
+  Icon,
+  Input,
+  Overlay,
+  Text,
+} from "react-native-magnus";
 import { resetPassword } from "../auth";
 import { Header } from "../components/Header";
 import { MainView, ScrollView, View } from "../components/Themed";
@@ -12,6 +21,15 @@ const User = ({ data, mutations }) => {
   const [name, setName] = React.useState(defaultName);
   const [emailSent, setEmailSent] = React.useState(false);
   const dropdownRef = React.createRef();
+
+  const [location, setLocation] = React.useState(null);
+  const [locationLoading, setLocationLoading] = React.useState(false);
+  const [locationOverlay, setLocationOverlay] = React.useState(false);
+  const useCurrentLocation = async () => {
+    await Location.requestForegroundPermissionsAsync();
+    const userLocation = await Location.getCurrentPositionAsync({});
+    setLocation(userLocation.coords);
+  };
 
   return (
     <View>
@@ -29,6 +47,23 @@ const User = ({ data, mutations }) => {
         ref={dropdownRef}
         title={
           <Div alignSelf="center">
+            <Div flexDir="row" justifyContent="space-between">
+              <Text mb={5} fontSize="sm">
+                {data.user.email}
+              </Text>
+              <Text
+                mb={5}
+                onPress={() => {
+                  if (emailSent) return;
+                  resetPassword(data.user.email).then(() => setEmailSent(true));
+                }}
+                textAlign="right"
+                color={emailSent ? "black" : "fruxgreen"}
+                fontSize="sm"
+              >
+                {emailSent ? "Mail Sent!" : "Reset password"}
+              </Text>
+            </Div>
             <Input
               placeholder="Username"
               w="65%"
@@ -56,21 +91,28 @@ const User = ({ data, mutations }) => {
               }
             />
             <Div flexDir="row" justifyContent="space-between">
-              <Text mt={10} fontSize="sm">
-                {data.user.email}
-              </Text>
-              <Text
-                mt={10}
-                onPress={() => {
-                  if (emailSent) return;
-                  resetPassword(data.user.email).then(() => setEmailSent(true));
-                }}
-                textAlign="right"
-                color={emailSent ? "black" : "fruxgreen"}
+              <Div></Div>
+              <Button
+                py={5}
+                px={0}
+                bg={undefined}
+                color="blue500"
+                underlayColor="blue100"
                 fontSize="sm"
+                onPress={() => {
+                  setLocationOverlay(true);
+                }}
+                suffix={
+                  <Icon
+                    name="location-outline"
+                    fontFamily="Ionicons"
+                    fontSize="xl"
+                    color="blue600"
+                  />
+                }
               >
-                {emailSent ? "Mail Sent!" : "Reset password"}
-              </Text>
+                {location ? "Change Location" : "Set Location"}
+              </Button>
             </Div>
           </Div>
         }
@@ -146,6 +188,79 @@ const User = ({ data, mutations }) => {
           </Button>
         </Dropdown.Option>
       </Dropdown>
+
+      <Overlay visible={locationOverlay}>
+        <MapView
+          initialRegion={{
+            latitude: location?.latitude || -34.5723074,
+            longitude: location?.longitude || -58.4346815,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.015,
+          }}
+          style={{ height: 400 }}
+          showsUserLocation={true}
+          onLongPress={(e) => setLocation(e.nativeEvent.coordinate)}
+        >
+          {location && <Marker coordinate={location} />}
+        </MapView>
+
+        <Div my={10} row justifyContent="space-between">
+          <Div alignSelf="flex-end">
+            <Button
+              py={0}
+              px={0}
+              bg={undefined}
+              color="gray800"
+              fontSize="md"
+              disabled={locationLoading}
+              onPress={async () => {
+                setLocationLoading(true);
+                await useCurrentLocation();
+                setLocationLoading(false);
+              }}
+              prefix={
+                <Icon
+                  name={locationLoading ? "spinner" : "location-outline"}
+                  fontFamily={locationLoading ? "EvilIcons" : "Ionicons"}
+                  fontSize="xl"
+                  color="gray800"
+                />
+              }
+            >
+              Use My Location
+            </Button>
+          </Div>
+
+          <Div row alignSelf="flex-end">
+            <Button
+              mx={5}
+              fontSize="sm"
+              p={8}
+              bg={undefined}
+              borderWidth={1}
+              borderColor="fruxgreen"
+              color="fruxgreen"
+              onPress={() => {
+                setLocationOverlay(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              onPress={() => {
+                setLocationOverlay(false);
+              }}
+              mx={5}
+              fontSize="sm"
+              p={8}
+              bg="fruxgreen"
+              color="white"
+            >
+              Save
+            </Button>
+          </Div>
+        </Div>
+      </Overlay>
     </View>
   );
 };
