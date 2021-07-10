1384c0bbc0b18c86d909b86b2210c88d73fa9217
FdelMazo <fdelmazo@fi.uba.ar>
2021-07-10 15:05:52 -0300

    Fix wrong view in loading screen

--- screens/Loading.jsx
+++ screens/Loading.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Linking, TouchableOpacity } from "react-native";
 import { Div, Image, Text } from "react-native-magnus";
-import { MainView, View } from "../components/Themed";
+import { View } from "../components/Themed";
 
 const Component = () => {
   const [wakeup, setWakeup] = React.useState(false);
@@ -18,42 +18,29 @@ const Component = () => {
 
   return wakeup ? (
     <View>
-      <MainView>
-        <Div justifyContent="center" h="100%">
-          <Image
-            resizeMode="contain"
-            h={350}
-            source={require("../assets/images/loading.png")}
-          />
-          <Div mt="md" alignItems="center">
-            <Text>Oops! It appears that our server is still sleeping!</Text>
-            <Text>
-              Just wait a few minutes and we'll be right back with you!
-            </Text>
-          </Div>
-          <Div
-            position="absolute"
-            bottom={0}
-            p="xl"
-            mx="xl"
-            row
-            flexWrap="wrap"
-          >
-            <Text fontSize="sm">
-              If you see this problem for longer than 5 minutes, please{" "}
-            </Text>
-            <TouchableOpacity
-              onPress={() => {
-                Linking.openURL(
-                  "https://github.com/camidvorkin/frux-app-server"
-                );
-              }}
-            >
-              <Text color="fruxgreen">contact us!</Text>
-            </TouchableOpacity>
-          </Div>
+      <Div justifyContent="center" h="100%">
+        <Image
+          resizeMode="contain"
+          h={350}
+          source={require("../assets/images/loading.png")}
+        />
+        <Div mt="md" alignItems="center">
+          <Text>Oops! It appears that our server is still sleeping!</Text>
+          <Text>Just wait a few minutes and we'll be right back with you!</Text>
         </Div>
-      </MainView>
+        <Div position="absolute" bottom={0} p="xl" mx="xl" row flexWrap="wrap">
+          <Text fontSize="sm">
+            If you see this problem for longer than 5 minutes, please{" "}
+          </Text>
+          <TouchableOpacity
+            onPress={() => {
+              Linking.openURL("https://github.com/camidvorkin/frux-app-server");
+            }}
+          >
+            <Text color="fruxgreen">contact us!</Text>
+          </TouchableOpacity>
+        </Div>
+      </Div>
     </View>
   ) : null;
 };
