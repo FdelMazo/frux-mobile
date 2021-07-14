b0ee2b65dcae0d4def110884f275fe3634d1f480
FdelMazo <fdelmazo@fi.uba.ar>
2021-07-14 18:21:46 -0300

    Ignore unfixable error

--- services/oauth.jsx
+++ services/oauth.jsx
@@ -7,6 +7,9 @@ import { LogBox } from "react-native";
 
 // https://stackoverflow.com/questions/44603362/setting-a-timer-for-a-long-period-of-time-i-e-multiple-minutes
 LogBox.ignoreLogs(["Setting a timer"]);
+LogBox.ignoreLogs([
+  "Can't perform a React state update on an unmounted component.",
+]);
 
 export async function redirectToGithub() {
   const returnUrl = makeRedirectUri({
