7a4fc6a3bf776102373836ee316f028a3b441541
FdelMazo <fdelmazo@fi.uba.ar>
2021-07-17 22:52:20 -0300

    Add signatures of chat service

---
+++ services/chat.jsx
@@ -0,0 +1,10 @@
+import { HeaderBackground } from "@react-navigation/stack";
+import * as Notifications from "expo-notifications";
+
+export const getProjectsConversations = async (project_id) => {};
+
+export const getUserConversations = async (user_id, project_id) => {};
+
+export const postQuestion = async (user_id, project_id, body) => {};
+
+export const postReply = async (user_id, question_id, body) => {};
